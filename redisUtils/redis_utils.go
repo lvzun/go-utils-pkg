bd146633b82911f8b2003f0e2ef8fb82ee901c75
worker11 <worker11@imk8.cn>
2025-05-29 16:44:19 +0800

    redis初始化更新

--- redisUtils/redis_utils.go
+++ redisUtils/redis_utils.go
@@ -6,7 +6,8 @@ import (
 )
 
 var (
-	Rdb *redis.ClusterClient
+	Rdb       *redis.ClusterClient
+	RdbSingle *redis.Client
 )
 
 func InitRedisCluster(addrList []string, auth string) {
@@ -16,5 +17,12 @@ func InitRedisCluster(addrList []string, auth string) {
 		DialTimeout:  time.Second * 10,
 		ReadTimeout:  time.Second * 10,
 		WriteTimeout: time.Second * 10,
+	})
+}
+func InitRedis(addr string, auth string, db int) {
+	RdbSingle = redis.NewClient(&redis.Options{
+		Addr:     addr, // Redis 服务器地址
+		Password: auth, // 密码，没有则为空
+		DB:       db,   // 使用默认 DB
 	})
 }
